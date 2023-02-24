5c733c9db990c5cf9a5f40503061d442a9848cc9
dirk-bester <dirk.bester@gmail.com>
2023-02-24 14:10:39 -0600

    Initial Orange Chicken

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import typescript from '@rollup/plugin-typescript';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), typescript()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
